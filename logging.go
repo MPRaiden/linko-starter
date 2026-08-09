c1fb4b87f8fa7d546e8e577378f64ef1c958f14e
Marin Peric Radan <marin.peric@seekandhit.com>
2026-08-09 15:03:33 +0200

    chore: log buffers cleanup logic

--- logging.go
+++ logging.go
@@ -8,23 +8,35 @@ import (
 	"os"
 )
 
-func initializeLogger() (*log.Logger, error) {
+type closeFunc func() error
+
+func initializeLogger() (*log.Logger, closeFunc, error) {
 	fileName, exists := os.LookupEnv("LINKO_LOG_FILE")
 	if exists {
 		logFile, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o644)
-		bufferedFile:= bufio.NewWriterSize(logFile, 8192)
+		bufferedFile := bufio.NewWriterSize(logFile, 8192)
 
 		if err != nil {
-			return nil, fmt.Errorf("failed to open file: %v", err)
+			return nil, nil, fmt.Errorf("failed to open file: %v", err)
 		}
 		multiWritter := io.MultiWriter(os.Stderr, bufferedFile)
 		logger := log.New(multiWritter, "", log.LstdFlags)
 
-		return logger, nil
+		closeLogger := func() error {
+			err := bufferedFile.Flush()
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+		return logger, closeLogger, nil
 	}
 
 	singleWritter := os.Stderr
 	logger := log.New(singleWritter, "", log.LstdFlags)
+	closeFuncNoOp := func() error {
+		return nil
+	}
 
-	return logger, nil
+	return logger, closeFuncNoOp, nil
 }
--- main.go
+++ main.go
@@ -25,7 +25,13 @@ func main() {
 }
 
 func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir string) int {
-	logger, err := initializeLogger()
+	logger, closeFunc, err := initializeLogger()
+	defer func() {
+		err := closeFunc()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to cleanup buffered logs: %v", err)
+		}
+	}()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to initialize logger: %v\n", err)
 		return 1
