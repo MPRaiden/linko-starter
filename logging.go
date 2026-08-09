d8b0c1f1ebe009e819290cd2498fa952dc0a4678
Marin Peric Radan <marin.peric@seekandhit.com>
2026-08-09 13:38:04 +0200

    chore: buffered logging

--- logging.go
+++ logging.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"log"
@@ -11,10 +12,12 @@ func initializeLogger() (*log.Logger, error) {
 	fileName, exists := os.LookupEnv("LINKO_LOG_FILE")
 	if exists {
 		logFile, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o644)
+		bufferedFile:= bufio.NewWriterSize(logFile, 8192)
+
 		if err != nil {
 			return nil, fmt.Errorf("failed to open file: %v", err)
 		}
-		multiWritter := io.MultiWriter(os.Stderr, logFile)
+		multiWritter := io.MultiWriter(os.Stderr, bufferedFile)
 		logger := log.New(multiWritter, "", log.LstdFlags)
 
 		return logger, nil
