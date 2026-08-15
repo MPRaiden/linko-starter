ac4c62fea3c5912dfddd1540de5b1bd6e4a4a929
mpr <marinpericradan1@gmail.com>
2026-08-15 12:49:03 +0200

    chore: group attributes slog

--- logging.go
+++ logging.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log/slog"
 	"os"
+
+	pkgerr "github.com/pkg/errors"
 )
 
 type closeFunc func() error
 
+type stackTracer interface {
+	error
+	StackTrace() pkgerr.StackTrace
+}
+
 func replaceAttr(groups []string, a slog.Attr) slog.Attr {
 	if a.Key == "error" {
-		err, ok := a.Value.Any().(error)
-		if !ok {
-			return a
+		err, _ := a.Value.Any().(error)
+		if stackErr, ok := errors.AsType[stackTracer](err); ok {
+			return slog.GroupAttrs("error", slog.Attr{
+				Key:   "message",
+				Value: slog.StringValue(stackErr.Error()),
+			}, slog.Attr{
+				Key:   "stack_trace",
+				Value: slog.StringValue(fmt.Sprintf("%+v", stackErr.StackTrace())),
+			})
 		}
-		return slog.String("error", fmt.Sprintf("%+v", err))
 	}
 	return a
 }
